aeea66b046f25e8c3a313a8e1baf85f6a4aaa97f
Logan Quon <Logan Quon>
2026-01-28 10:58:35 -0800

    file added

---
+++ MyProgram.java
@@ -0,0 +1,17 @@
+public class MyProgram {
+    public static void main(String[] args) {
+        // Define 5 double values
+        double value1 = 12.5;
+        double value2 = 18.3;
+        double value3 = 14.7;
+        double value4 = 22.1;
+        double value5 = 16.4;
+        
+        // Calculate the average
+        double average = (value1 + value2 + value3 + value4 + value5) / 5.0;
+        
+        // Display the values and average
+        System.out.println("Values: " + value1 + ", " + value2 + ", " + value3 + ", " + value4 + ", " + value5);
+        System.out.println("Average: " + average);
+    }
+}
